1ee44cee3f04c236eac887002feb7e0258bf29b6
simon-schaefer <simon.k.schaefer@gmail.com>
2024-12-23 11:00:12 -0800

    [NeoVim] Added cheatsheet

--- nvim/lua/custom/plugins/cheatsheet.lua
+++ nvim/lua/custom/plugins/cheatsheet.lua
@@ -11,5 +11,48 @@ return {
 			"                                      ",
 			"                                      ",
 		},
+		keymaps = {
+			["Buffers/Tabs"] = {
+				{ "Close buffer without saving", "bd" },
+			},
+			["Navigation"] = {
+				{ "Move left", "h" },
+				{ "Move down", "j" },
+				{ "Move up", "k" },
+				{ "Move right", "l" },
+				{ "Move to next word", "w" },
+				{ "Move to previous word", "b" },
+				{ "Move to start of line", "0" },
+				{ "Move to end of line", "$" },
+				{ "Go to top of file", "gg" },
+				{ "Go to bottom of file", "G" },
+				{ "Go to specific line", ":{number}" },
+			},
+			["Editing"] = {
+				{ "Enter insert mode at cursor", "i" },
+				{ "Append (enter Insert mode after cursor)", "a" },
+				{ "Open new line below", "o" },
+				{ "Open new line above", "O" },
+				{ "Delete line", "dd" },
+				{ "Undo last change", "u" },
+				{ "Redo last undone change", "<C-r>" },
+			},
+			["Search"] = {
+				{ "Search forward", "/" },
+				{ "Search backward", "?" },
+				{ "Next search result", "n" },
+				{ "Previous search result", "N" },
+			},
+			["File Operations"] = {
+				{ "Save file", ":w" },
+				{ "Quit Neovim", ":q" },
+				{ "Save and quit", ":wq" },
+				{ "Open and edit file", ":e {filename}" },
+			},
+			["Advanced Features"] = {
+				{ "Enter Visual mode", "v" },
+				{ "Enter Visual Block mode", "V" },
+			},
+		},
 	},
 }
