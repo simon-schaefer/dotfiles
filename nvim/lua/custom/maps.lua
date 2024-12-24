a259904db590f11a46daa9cc5eec696b6ef8c4a5
simon-schaefer <simon.k.schaefer@gmail.com>
2024-12-24 00:45:45 -0800

    [NeoVim] Added git fugitive as interface

--- nvim/lua/custom/maps.lua
+++ nvim/lua/custom/maps.lua
@@ -2,11 +2,14 @@ local wk = require("which-key")
 local nvcheatsheet = require("nvcheatsheet")
 
 wk.add({
-	{ "<leader>f", group = "file" }, -- group
+	{ "<leader>f", group = "file" },
 	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
 	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep", mode = "n" },
 	{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers", mode = "n" },
-	{ "<leader>fn", desc = "New File" },
+
+	{ "<leader>g", group = "git" },
+	{ "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", desc = "View git changes inline", mode = "n" },
+
 	{ "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
 	{
 		"<leader>b",
