950f78c24d1bfd853fed39c30d64070535d7a9d2
simon-schaefer <simon.k.schaefer@gmail.com>
2024-12-26 20:28:35 -0800

    [NeoVim] Added toggle for file tree & fzf with symbolic links

--- nvim/lua/custom/maps.lua
+++ nvim/lua/custom/maps.lua
@@ -2,15 +2,18 @@ local wk = require("which-key")
 local nvcheatsheet = require("nvcheatsheet")
 
 wk.add({
+	-- Telescope shortcuts.
 	{ "<leader>f", group = "file" },
-	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+	-- follow=true enables to trace symbolic links in the working directory.
+	{ "<leader>ff", "<cmd>Telescope find_files follow=true<cr>", desc = "Find File", mode = "n" },
 	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep", mode = "n" },
 	{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers", mode = "n" },
 
+	-- Git shortcuts.
 	{ "<leader>g", group = "git" },
 	{ "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", desc = "View git changes inline", mode = "n" },
 
-	{ "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+	-- Buffer overview shortcut.
 	{
 		"<leader>b",
 		group = "buffers",
@@ -24,7 +27,13 @@ wk.add({
 		{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
 	},
 
+	-- Neotree shortcut.
+	{ "<leader>t", ":Neotree toggle<cr>", desc = "Toggle file tree", mode = "n" },
+
+	-- Tab navigation shortcut.
 	{ "<Tab>", ":bnext<cr>", desc = "Next Tab", mode = "n" },
 	{ "<S-Tab>", ":bprevious<cr>", desc = "Previous Tab", mode = "n" },
+
+	-- Cheatsheet shortcut.
 	{ "<leader>?", nvcheatsheet.toggle, desc = "Cheatsheet" },
 })
