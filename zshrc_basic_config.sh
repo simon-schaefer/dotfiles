ae62eed417598131a5d0308b3048eb308875605a
Simon Hubertus Schaefer <sischaef@meta.com>
2025-02-08 12:19:08 -0800

    [ZSH] Only activate zsh if installed

--- zshrc_basic_config.sh
+++ zshrc_basic_config.sh
@@ -1,8 +1,10 @@
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
-export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="eastwood"
-plugins=(git)
-source $ZSH/oh-my-zsh.sh
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    export ZSH="$HOME/.oh-my-zsh"
+    ZSH_THEME="eastwood"
+    plugins=(git)
+    source $ZSH/oh-my-zsh.sh
+fi
 
 # Conda setup.
 launch-conda() {
