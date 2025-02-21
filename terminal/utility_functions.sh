5ff7157b6162a17c319b79569d9b55076282c874
simon-schaefer <simon.k.schaefer@gmail.com>
2025-02-20 17:52:13 -0800

    [ZSH] merge-directories() utility function

--- terminal/utility_functions.sh
+++ terminal/utility_functions.sh
@@ -31,3 +31,37 @@ apply-function-to-files() {
     fi
     done
 }
+
+
+## Merge several directories to a single output directory by prefixing the directory name.
+# Arguments:
+#   $1...N - Input directories.
+#   $N - Output directory.
+#
+# Returns:
+#   - 0 on success.
+#   - 1 on error (invalid arguments, no files found, or directory issues).
+#
+# Example:
+#   merge-directories dir1/ dir2/ output-dir/
+merge-directories() {
+    local output_dir="${@: -1}"
+    local input_dirs=("${@:1:${#@}-1}")
+
+    if [ ! -d "$output_dir" ]; then
+        mkdir -p "$output_dir"
+    fi
+
+    for dir in "${input_dirs[@]}"; do
+        if [ ! -d "$dir" ]; then
+            echo "error: input directory '$dir' does not exist."
+            continue
+        fi
+
+        for file in "$dir"/*; do
+            filename=$(basename "$file")
+            newfilename="${dir%/}-${filename}"
+            cp "$file" "${output_dir}/${newfilename##*/}"
+        done
+    done
+}
