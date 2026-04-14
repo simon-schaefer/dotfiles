8f9311b30233db294e779c842d8a48b1644a7689
simon-schaefer <simon.k.schaefer@gmail.com>
2026-04-14 11:33:40 +0200

    [NVIM] Enforce usage of pyproject.yaml

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,15 @@
 require("config.settings")
 require("config.lazy")
+
+-- Enforce the usage of pyproject yaml
+local util = require("conform.util")
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "isort", "black" },
+	},
+	formatters = {
+		black = {
+			cwd = util.root_file({ "pyproject.toml", ".git" }),
+		},
+	},
+})
